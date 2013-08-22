e31370749462fc936dbf373326160c9f281634cb
Carlos Omar Briseno Gutierrez <omark7c1@gmail.com>
2013-08-22 11:56:34 -0500

    show location plugin_dir of mysql

---
+++ usr/local/lib/shellmin/CentOS/generic/mysql/showPluginDir.bash
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# check if executed by root
+if [[ "$(id -u)" != "0" ]]; then
+    echo "This script must be run as root";
+    exit 1;
+fi
+
+sHelp=$(cat << NEOF 
+
+#Example
+    shellmin mysql showPluginDir
+    shellmin mysql showPluginDir
+NEOF
+)
+
+# check for .my.cnf
+if [[ ! -f ~/.my.cnf ]]; then
+    echo "The .my.cnf file has not been set. Please, read enclosed documentation to clarify.";
+    exit 1;
+fi
+
+#do
+mysql --defaults-extra-file=~/.my.cnf -B -N -e "show variables like 'plugin_dir';"
+
+exit 0
