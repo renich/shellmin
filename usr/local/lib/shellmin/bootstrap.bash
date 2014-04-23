6940cb40f90b7f531a11d98d4d6be0c83516d0a0
Renich Bon Ciric <renich@woralelandia.com>
2014-04-23 05:48:49 -0500

    3-level helpers
    Now, helpers should be available in 3 levels: general, distro-specific and distro-version-specific

--- usr/local/lib/shellmin/bootstrap.bash
+++ usr/local/lib/shellmin/bootstrap.bash
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
+source $( readlink -m "$( dirname "${BASH_SOURCE}" )" )/env.bash
+
 # get helpers
-for helper in $( find -P "${SHELLMIN_HELPERSDIR}" -mindepth 1 -maxdepth 1 -type f ); do
-    source "${helper}"
+for path in "${SHELLMIN_HELPERSDIR[@]}"; do
+    for helper in $( find -P "${path}" -mindepth 1 -maxdepth 1 -type f ); do
+		source "${helper}"
+	done
 done
 
 # get controllers
 for path in "${SHELLMIN_CONTROLLERSDIR[@]}"; do
-    for controller in $( find -P "${path}" -mindepth 1 -maxdepth 1 -type d ); do
+    for controller in $( find -P "${path}" -mindepth 1 -maxdepth 1 -type d -not -iname 'helpers' ); do
         SHELLMIN_CONTROLLERS+="${controller} ";
     done
 done
 
 export SHELLMIN_CONTROLLERS;
+export SHELLMIN_HELPERS;
